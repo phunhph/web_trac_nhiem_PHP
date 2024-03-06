589f461a8dcfde4e7f470552c1abe06f26d65e61
Nguyen huu phu <phub3@gmail.com>
2024-03-06 19:11:27 +0700

    commit utf8

--- dao/config.php
+++ dao/config.php
@@ -11,10 +11,10 @@ class DatabaseConnection
     public function __construct()
     {
         try {
-            $this->conn = new \PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+            $this->conn = new \PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf8mb4", $this->username, $this->password);
             $this->conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
+            echo "Kết nối không thành công: " . $e->getMessage();
         }
     }
 
@@ -23,28 +23,3 @@ class DatabaseConnection
         return $this->conn;
     }
 }
-// <?php
-
-// class DatabaseConnection
-// {
-//     private $host = 'localhost:3306';
-//     private $dbname = 'bee97534_thi_tin_hoc';
-//     private $username = 'bee97534_admin';
-//     private $password = 'ph#241Es1';
-//     private $conn;
-
-//     public function __construct()
-//     {
-//         try {
-//             $this->conn = new \PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
-//             $this->conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-//         } catch (\PDOException $e) {
-//             echo "Connection failed: " . $e->getMessage();
-//         }
-//     }
-
-//     public function getConnection()
-//     {
-//         return $this->conn;
-//     }
-// }
