c9647ceb5f67b7d8ea92f63e119053d3e9d7fc2c
Nguyen huu phu <phub3@gmail.com>
2024-03-06 18:40:57 +0700

    .

--- dao/quanlymonthiDAO.php
+++ dao/quanlymonthiDAO.php
@@ -158,7 +158,7 @@ class QuanLyMonThiDAO
         while ($row = $stmt->fetch(\PDO::FETCH_OBJ)) {
             $noiDungThi[] = $row; // Thêm dữ liệu vào mảng
         }
-        $stmt->debugDumpParams();
+
         return $noiDungThi;
     }
     public function getmobodevaten($mamodun)
